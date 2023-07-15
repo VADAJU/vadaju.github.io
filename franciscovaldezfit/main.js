cd95ae6714e32ff6481f9753a867502507a9d03d
VADAJU <vadaju.contacto@gmail.com>
2023-07-15 13:32:36 -0600

    se pusieron los botones naranjas y se cambiaron los modales

--- franciscovaldezfit/main.js
+++ franciscovaldezfit/main.js
@@ -17,23 +17,23 @@ window.onscroll = () => {
     });
 };
 
-function copiaralportapapeles1(){
-    let texto1 = document.getElementById('texto1');
-    texto1.select();
-    texto1.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-}
+// function copiaralportapapeles1(){
+//     let texto1 = document.getElementById('texto1');
+//     texto1.select();
+//     texto1.setSelectionRange(0, 99999);
+//     document.execCommand('copy');
+// }
 
-function copiaralportapapeles2(){
-    let texto2 = document.getElementById('texto2');
-    texto2.select();
-    texto2.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-}
+// function copiaralportapapeles2(){
+//     let texto2 = document.getElementById('texto2');
+//     texto2.select();
+//     texto2.setSelectionRange(0, 99999);
+//     document.execCommand('copy');
+// }
 
-function copiaralportapapeles3(){
-    let texto3 = document.getElementById('texto3');
-    texto3.select();
-    texto3.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-}
+// function copiaralportapapeles3(){
+//     let texto3 = document.getElementById('texto3');
+//     texto3.select();
+//     texto3.setSelectionRange(0, 99999);
+//     document.execCommand('copy');
+// }
